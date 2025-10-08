7d245e878b5ec97402a67ba168a95d032bd9b7b4
Artasi67 <athurtavaressilva@mail.com>
2025-10-08 07:43:32 -0300

    vamo ve se funfa

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Olá mundo! App Etec Delivery - app de controle de entregas para entregadores</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
